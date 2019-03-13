b564cf8a2fe59cc380533272a7d124b9b7ff9062
Matvey Korbut <korbutmatvey@gmail.com>
2019-03-13 13:34:23 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrayOfBrackets=str.split('');
+  let config=bracketsConfig;
+
+  for (var i=0; i<arrayOfBrackets.length;i++){
+       for (var j=0; j<config.length; j++){
+           let openBracket = config[j][0];
+           let closeBracket = config[j][1]
+           if((arrayOfBrackets[i]===openBracket) && (arrayOfBrackets[i+1]===closeBracket)){
+               arrayOfBrackets.splice(i,2);
+               i=-1;
+           }
+           if (arrayOfBrackets.length==0){
+               return true;
+               }
+       }
+  }
+  return false;
 }
